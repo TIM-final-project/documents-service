33095f791b573cdcfe588831485e7a079140c4d8
Tomas Luna <tomas_luna1996@hotmail.com>
2023-09-02 21:21:44 -0300

    fix documents update state missing

--- src/utils/documents.ts
+++ src/utils/documents.ts
@@ -3,7 +3,11 @@ import { States } from 'src/enums/states.enum';
 export function isValidStateUpdate(prev: States, next: States): boolean {
   switch (prev) {
     case States.PENDING:
-      return next === States.ACCEPTED || next === States.REJECTED;
+      return (
+        next === States.ACCEPTED ||
+        next === States.REJECTED ||
+        next === States.EXPIRED
+      );
     case States.EXPIRED:
       return next === States.PENDING;
     case States.ACCEPTED:
